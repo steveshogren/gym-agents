38eaf441446aba42bc5c32f56123c3b686512482
jack <steve.a.shogren@gmail.com>
2017-12-08 05:48:17 -0500

    Cleaning up and moving stuff around

--- random_agent.py
+++ random_agent.py
@@ -5,7 +5,6 @@ import sys
 import gym
 from gym import wrappers
 from tabular_q_agent import TabularQAgent
-import pickle as pickle
 import helpers as h
 
 # gutted to use TabularQAgent instead with the Reverse-v0
@@ -18,19 +17,10 @@ class RandomAgent(object):
         # print str(self.action_space)
         return self.action_space.sample()
 
-def saveDict(defaultdic):
-    output = open('data.pkl', 'wb')
-    d = dict(defaultdic)
-    pickle.dump(d, output)
-    output.close()
-    print(d)
-
-
 if __name__ == '__main__':
     env = gym.make('Reverse-v0')
 
     env.seed(0)
-    #agent = TabularQAgent(ob, env.action_space)
 
     episode_count = 100
     reward = 0
@@ -40,37 +30,11 @@ if __name__ == '__main__':
     # convert the tuple action space into Discrete
     perms = h.triplePerms(env.action_space)
     actionSize = h.tupleSize(env.action_space)
-    #while True:
 
     agent = TabularQAgent(ob, actionSize)
-    #for i in range(episode_count):
-            # reconvert the Discrete action into a tuple for the AlgorithmicEnv
 
-    output = open('data.pkl', 'rb')
-    data1 = pickle.load(output)
-    output.close()
-    print(data1)
-    if (data1):
-        agent.makeDefaultDict(data1)
+    agent.learn(env, lambda action: env.step(perms[action]))
 
-    agent.learn(env, lambda action: env.step(perms[action]),
-                lambda dic: saveDict(dic))
-
-    saveDict(agent.q)
-    output.close()
-        #action = agent.act(ob)
-            #ob, reward, done, _ = env.step(action)
-            #if done:
-            #    break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
-
-    # Close the env and write monitor result info to disk
     env.render()
     env.close()
 
-    # Upload to the scoreboard. We could also do this from another
-    # process if we wanted.
-    #logger.info("Successfully ran RandomAgent. Now trying to upload results to the scoreboard. If it breaks, you can always just try re-uploading the same results.")
-    #gym.upload(outdir)
--- tabular_q_agent.py
+++ tabular_q_agent.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from gym import error
 from gym.spaces import discrete
 import argparse, numpy as np
+import pickle as pickle
 
 class TabularQAgent(object):
     """
@@ -17,15 +18,31 @@ class TabularQAgent(object):
         self.config = {
             "init_mean" : 0.0,      # Initialize Q values with this mean
             "init_std" : 0.0,       # Initialize Q values with this standard deviation
-            "learning_rate" : 1.0,  # learning rate 1.0 - 0.0  where 1.0 is for perfectly deterministic scenarios
-            "eps": 0.75,            # Epsilon in epsilon greedy policies - 1.0 infinitely long negative traits
-            "discount": 0.95,
+            "learning_rate" : 0.2,  # learning rate 1.0 - 0.0  where 1.0 is for perfectly deterministic scenarios
+            "eps": 0.90,            # Epsilon in epsilon greedy policies - 1.0 infinitely long negative traits
+            "discount": 0.90,
             "n_iter": 10000000}        # Number of iterations
         self.config.update(userconfig)
-        self.q = defaultdict(lambda: self.config["init_std"] * np.random.randn(self.action_n) + self.config["init_mean"])
+        self.makeDefaultDict()
+        #self.q = defaultdict(lambda: self.config["init_std"] * np.random.randn(self.action_n) + self.config["init_mean"])
 
-    def makeDefaultDict(self, init):
-        self.q = defaultdict(lambda: self.config["init_std"] * np.random.randn(self.action_n) + self.config["init_mean"], init)
+
+    def makeDefaultDict(self):
+        output = open('data.pkl', 'rb')
+        data1 = pickle.load(output)
+        output.close()
+        print(data1)
+        if (data1):
+            self.q = defaultdict(lambda: self.config["init_std"] * np.random.randn(self.action_n) + self.config["init_mean"], data1)
+        else:
+            self.q = defaultdict(lambda: self.config["init_std"] * np.random.randn(self.action_n) + self.config["init_mean"])
+
+    def saveState(self):
+        output = open('data.pkl', 'wb')
+        d = dict(self.q)
+        pickle.dump(d, output)
+        output.close()
+        print(d)
 
     def chooseAction(self, observation, eps=None):
         if eps is None:
@@ -33,7 +50,7 @@ class TabularQAgent(object):
         # epsilon greedy.
         return np.argmax(self.q[observation]) if np.random.random() > eps else self.action_space.sample()
 
-    def learn(self, env, envStep, saveState):
+    def learn(self, env, envStep):
         config = self.config
         obs = env.reset()
         q = self.q
@@ -53,7 +70,7 @@ class TabularQAgent(object):
             if done:
                 currentSize = currentSize + 1
                 if currentSize % 10000 == 0:
-                    saveState(q)
+                    self.saveState()
                     print (str(currentSize) + "/" + str(config["n_iter"]))
                     env.render()
                 # either a failure or success, reset the env
